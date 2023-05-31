ee18c6ad912ea6a59ff22b7127fc8df06aee3209
Randy MS <randyma01@gmail.com>
2023-05-31 14:39:36 -0600

    feat (section 1-2): hero editor and display

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+import { Hero } from '../models/hero';
+import { HEROES } from '../data/mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+})
+export class HeroesComponent {
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
