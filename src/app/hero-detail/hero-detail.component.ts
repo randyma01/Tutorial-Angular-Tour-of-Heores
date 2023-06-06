270db8d2e88820514116efe8a945479035731321
Randy MS <randyma01@gmail.com>
2023-06-06 17:04:07 -0600

    feat (section-5): add navigation

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,9 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, Input } from '@angular/core';
+import { Location } from '@angular/common';
 
 import { Hero } from '../models/hero';
+import { HeroService } from '../services/hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -9,4 +12,23 @@ import { Hero } from '../models/hero';
 })
 export class HeroDetailComponent {
   @Input() hero?: Hero;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
